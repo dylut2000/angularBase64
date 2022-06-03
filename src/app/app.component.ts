2fa3628f537a7a1600f1207cac31c032b1b5b346
hardy lutula <hardylutula@gmail.com>
2022-06-03 12:17:08 +0200

    ...lol

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ngBase64App';
+
+  isImageSaved: boolean = false;
+  cardImageBase64: string = '';
+
+  constructor() {}
+
+  CreateBase64String(fileInput: any) {
+    if (fileInput.target.files && fileInput.target.files[0]) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        const image = new Image();
+        image.src = e.target.result;
+        image.onload = rs => {
+          const imgBase64Path = e.target.result;
+          this.cardImageBase64 = imgBase64Path;         
+          this.isImageSaved = true;
+          console.log(imgBase64Path);
+        };
+      };
+      reader.readAsDataURL(fileInput.target.files[0]);
+    }
+  }
 }
